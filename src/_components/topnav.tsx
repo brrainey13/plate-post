e109c4b98bef25915152615abe442de8207b9c4d
brrainey13 <brrainey13@gmail.com>
2024-04-21 18:22:26 -0400

    setting up auith

--- src/_components/topnav.tsx
+++ src/_components/topnav.tsx
@@ -3,7 +3,7 @@ import { SignInButton, SignedOut, UserButton, SignedIn } from "@clerk/nextjs";
 export function TopNav() {
   return (
     <nav className="flex items-center justify-between w-full border-b p-4 text-xl font-semibold">
-      <div>Top Nav</div>
+      <div>Plate Post</div>
 
       <div>
         <SignedOut>
