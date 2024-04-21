939a6b33dc6649f07b329d74ed01b0d99bf681c6
brrainey13 <brrainey13@gmail.com>
2024-04-21 17:21:08 -0400

    DB committ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/d6fa6663-8332-4019-8bb9-5ffb218df637-f0urtj.png",
@@ -12,10 +13,21 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany()
+
+  console.log(posts)
+
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48 gap-4">
+            {post.name}
+          </div>
+        ))}
         {[...mockImages,...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48 gap-4">
             <img src={image.url} alt="image" />
